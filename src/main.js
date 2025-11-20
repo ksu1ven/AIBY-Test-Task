ccac7beb3439ce6a66861d0aac3dd682ee9ddb9c
Oksana Yahimovich <101278229+ksu1ven@users.noreply.github.com>
2025-11-20 12:38:27 +0300

    finish languages

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import "./styles/main.css";
 const fillPageContent = async () => {
   const supportedLangs = ["en", "de", "es", "fr", "ja", "pt"];
 
-  const constantValues = {
-    pricePerYear: "$39.99",
-    pricePerWeekOptionYear: "$0.48",
-    pricePerWeekOptionWeek: "$6.99",
-  };
-
   const urlParams = new URLSearchParams(window.location.search);
   let lang = urlParams.get("lang");
 
@@ -19,26 +13,25 @@ const fillPageContent = async () => {
   if (!supportedLangs.includes(lang)) {
     lang = "en";
   }
+  document.documentElement.setAttribute("lang", lang);
+  document.body.classList.add(`lang-${lang}`);
 
-  let langStrings;
+  let langJson;
   try {
-    langStrings = (await import(`./i18n/${lang}.json`)).default;
+    langJson = (await import(`./i18n/${lang}.json`)).default;
   } catch (e) {
-    langStrings = (await import(`./i18n/en.json`)).default;
+    langJson = (await import(`./i18n/en.json`)).default;
   }
 
   document.querySelectorAll("[data-i18n]").forEach((el) => {
     const key = el.getAttribute("data-i18n");
-    if (langStrings[key]) {
-      let text = langStrings[key] || "";
-
+    if (langJson[key]) {
+      let text = langJson[key] || "";
       if (text.includes("{{")) {
         text = text.replace(/{{\s*([\w-]+)\s*}}/g, (_, attrName) => {
-          const key = el.getAttribute(`data-${attrName}`) || "";
-          return constantValues[key];
+          return el.dataset[attrName] || "";
         });
       }
-
       el.innerHTML = text;
     }
   });
